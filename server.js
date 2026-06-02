5849b02eb9f123f75daf524049649cda390794df
Marcos Letty <marcos.letty@outlook.com>
2026-06-02 14:54:37 -0300

    fix: adiciona rota raiz para servir o index.html

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const conectarBanco = require('./config/database');
 const rotasItens = require('./routes/itemRoutes');
+const path = require('path');
 
 const app = express();
 const porta = process.env.PORT || 3000;
@@ -9,6 +10,11 @@ const porta = process.env.PORT || 3000;
 // Configurações base
 app.use(express.json());
 app.use(express.static('public'));
+
+// Define a rota principal:
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Inicia a conexão com o MongoDB Atlas
 conectarBanco();
